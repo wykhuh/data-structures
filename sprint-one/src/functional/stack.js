398c186bf52aad1aee863decbbffd4cd900748c8
James H. Edwards <james.howe.edwards@gmail.com>
2014-07-23 11:55:54 -0700

    finished functional stack

--- sprint-one/src/functional/stack.js
+++ sprint-one/src/functional/stack.js
@@ -3,15 +3,25 @@ var makeStack = function(){
 
   // Use an object with numeric keys to store values
   var storage = {};
+  var length = 0;
 
   // Implement the methods below
   someInstance.push = function(value){
+    storage[length] = value;
+    length +=1;
   };
 
   someInstance.pop = function(){
+    if(length > 0){
+      var result = storage[length-1];
+      storage[length] = undefined;
+      length -= 1;
+      return result;
+    }
   };
 
   someInstance.size = function(){
+    return length;
   };
 
   return someInstance;
