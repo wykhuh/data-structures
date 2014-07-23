025bd248fcf64edf2beee4ab855f1bcc1422d6e2
James H. Edwards <james.howe.edwards@gmail.com>
2014-07-23 12:24:05 -0700

    func-shared queue except error throwing

--- sprint-one/src/functional-shared/queue.js
+++ sprint-one/src/functional-shared/queue.js
@@ -1,9 +1,32 @@
 var makeQueue = function(){
-  // Hey! Rewrite in the new style. Your code will wind up looking very similar,
-  // but try not not reference your old code in writing the new style.
+  var queue = {};
+  queue.beg = 0;
+  queue.end = 0;
+  queue.storage = {};
+  queue.enqueue = queueMethods.enqueue;
+  queue.dequeue = queueMethods.dequeue;
+  queue.size = queueMethods.size;
+
+  return queue;
 };
 
-var queueMethods = {};
+var queueMethods = {
+  enqueue: function(value){
+    this.storage[this.end] = value;
+    this.end += 1;
+  },
+  dequeue: function(){
+    if(this.beg < this.end){
+      this.beg += 1;
+      return this.storage[this.beg-1];
+    } else {
+      throw {error:'whoa man!'};
+    }
+  },
+  size: function(){
+    return this.end - this.beg;
+  }
+};
 
 
 
