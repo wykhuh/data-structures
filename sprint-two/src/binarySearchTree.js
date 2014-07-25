5bdcb9b8e8621a8c081dd1a9e6dfd40d1e0601cc
wykhuh <accounts@hackreactor.com>
2014-07-25 15:56:13 -0700

    binary tree and graph finished

--- sprint-two/src/binarySearchTree.js
+++ sprint-two/src/binarySearchTree.js
@@ -1,6 +1,60 @@
 var makeBinarySearchTree = function(value){
+  var obj = {};
+  obj.value = value;
+  obj.left = null;
+  obj.right = null;
+  obj.insert = BinarySearchTreeMethods.insert;
+  obj.contains = BinarySearchTreeMethods.contains;
+  obj.depthFirstLog = BinarySearchTreeMethods.depthFirstLog;
+  return obj;
 };
 
+var BinarySearchTreeMethods = {};
+
+BinarySearchTreeMethods.insert = function(value){
+  if (value < this.value) {
+    if (this.left === null) {
+      this.left = makeBinarySearchTree(value);
+    } else {
+      this.left.insert(value);
+    }
+  } else {
+    if (this.right === null) {
+      this.right = makeBinarySearchTree(value);
+    } else {
+      this.right.insert(value);
+    }
+  }
+};
+
+BinarySearchTreeMethods.contains = function(value){
+  if(value === this.value) {
+    return true;
+  } else if (value < this.value ){
+    if (this.left === null) {
+      return false;
+    }
+    return  this.left.contains(value);
+  } else {
+    if(this.right === null) {
+      return false;
+    }
+    return this.right.contains(value);
+  }
+
+};
+BinarySearchTreeMethods.depthFirstLog = function(cb){
+
+  cb(this.value);
+
+  if (this.right){
+    this.right.depthFirstLog(cb);
+  }
+  if (this.left){
+    this.left.depthFirstLog(cb);
+  }
+
+};
 
 /*
  * Complexity: What is the time complexity of the above functions?
--- sprint-two/src/graph.js
+++ sprint-two/src/graph.js
@@ -1,24 +1,58 @@
 var Graph = function(){
+  this.nodes = {};
 };
 
 Graph.prototype.addNode = function(newNode, toNode){
+  this.nodes[newNode] = new Node(newNode);
+  if(Object.keys(this.nodes).length === 2){
+    var firstNode = Object.keys(this.nodes)[0];
+    this.addEdge(firstNode, newNode);
+  }
+  if(toNode !== undefined){
+    this.nodes[toNode] = new Node(toNode);
+    this.addEdge(newNode, toNode);
+  }
 };
 
 Graph.prototype.contains = function(node){
+  return (this.nodes[node] !== undefined);
 };
 
 Graph.prototype.removeNode = function(node){
+  for(var other in this.nodes[node].connectedTo){
+    this.removeEdge(node, other);
+  }
+  delete this.nodes[node];
 };
 
 Graph.prototype.getEdge = function(fromNode, toNode){
+  return this.nodes[fromNode].connectedTo[toNode] !== undefined;
 };
 
 Graph.prototype.addEdge = function(fromNode, toNode){
+  this.nodes[fromNode].connectedTo[toNode] = this.nodes[toNode];
+  this.nodes[toNode].connectedTo[fromNode] = this.nodes[fromNode];
+
 };
 
 Graph.prototype.removeEdge = function(fromNode, toNode){
+  if (this.getEdge(fromNode, toNode)) {
+    delete this.nodes[fromNode].connectedTo[toNode];
+    delete this.nodes[toNode].connectedTo[fromNode];
+  }
+  if(Object.keys(this.nodes[fromNode].connectedTo).length < 1) {
+    this.removeNode(fromNode);
+  }
+  if(Object.keys(this.nodes[toNode].connectedTo).length < 1) {
+    this.removeNode(toNode);
+  }
 };
 
 /*
  * Complexity: What is the time complexity of the above functions?
  */
+
+var Node = function(value) {
+  this.connectedTo = {};
+  this.value = value;
+};
