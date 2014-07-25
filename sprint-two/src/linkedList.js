671cddfd7bddc108500a5e888963016ce2098bb8
wykhuh <accounts@hackreactor.com>
2014-07-24 19:14:54 -0700

    linked to perfection

--- sprint-two/src/linkedList.js
+++ sprint-two/src/linkedList.js
@@ -4,12 +4,33 @@ var makeLinkedList = function(){
   list.tail = null;
 
   list.addToTail = function(value){
+    if(list.head === null){
+      var node = makeNode(value);
+      list.head = node;
+      list.tail = node;
+    } else {
+      var node = makeNode(value);
+      list.tail.next = node;
+      list.tail = node;
+    }
   };
 
   list.removeHead = function(){
+    var tem = list.head.value;
+    list.head =  list.head.next;
+    return tem;
   };
 
   list.contains = function(target){
+    var result = false;
+    var current = list.head;
+    while(current){
+      if(target === current.value){
+        result = true;
+      }
+      current = current.next;
+    }
+    return result;
   };
 
   return list;
