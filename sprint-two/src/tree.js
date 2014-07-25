b849f9b1852edfceab48a6dc4c6ecc004a8d0129
wykhuh <accounts@hackreactor.com>
2014-07-24 19:50:42 -0700

    tree is done

--- sprint-two/src/tree.js
+++ sprint-two/src/tree.js
@@ -2,6 +2,8 @@ var makeTree = function(value){
   var newTree = {};
   newTree.value = value;
   newTree.children = undefined;
+  newTree.addChild = treeMethods.addChild;
+  newTree.contains = treeMethods.contains;
   return newTree;
 };
 
@@ -11,10 +13,31 @@ var makeTree = function(value){
 var treeMethods = {};
 
 treeMethods.addChild = function(value){
-
+  if(this.children === undefined) {
+    this.children = [];
+  }
+  this.children.push(makeTree(value));
 };
 
 treeMethods.contains = function(target){
+  var tree = this;
+  var found = false;
+  var search = function(tree, target, result) {
+    //debugger;
+    if(tree.value === target){
+      found = true;
+      return;
+    } else if (tree.children === undefined) {
+      return;
+    } else {
+      for(var i=0; i <tree.children.length; i++) {
+        search(tree.children[i], target, found);
+      }
+    }
+    return found;
+  };
+
+  return search(tree, target, found);
 
 };
 
